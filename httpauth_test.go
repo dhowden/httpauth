46c8341e22d3bea4b49beeefaa2e6e96f3a36fc8
David Howden <dhowden@gmail.com>
2018-04-27 10:38:56 +1000

    update tests to run in seperate pkg

--- httpauth_test.go
+++ httpauth_test.go
@@ -2,12 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package httpauth
+package httpauth_test
 
 import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	. "github.com/dhowden/httpauth"
 )
 
 func TestCreds(t *testing.T) {
